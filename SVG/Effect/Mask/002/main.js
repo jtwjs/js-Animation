163a5bb029a4cad0cdece2ada2ac8e7f34202c5c
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2020-12-22 18:03:59 +0900

    SVG: Mask/002

--- SVG/Effect/Mask/002/main.js
+++ SVG/Effect/Mask/002/main.js
@@ -7,8 +7,10 @@
     const viewBoxHeight = viewBox[3];
     const widthRatio =  viewBoxWidth / window.innerWidth ;
     const heightRatio = viewBoxHeight / window.innerHeight;
+    console.log(viewBoxHeight,innerHeight);
     window.addEventListener('mousemove', (e) => {
-        
+        console.log(e.clientY);
+        console.log(e.clientY*heightRatio);
         magnifierElem.style.transform = `translate(${e.clientX * widthRatio }px, ${e.clientY * heightRatio}px)`;
         maskElem.style.transform = `translate(${e.clientX * widthRatio}px, ${e.clientY * heightRatio}px)`;
     });
